e34aeaeb15cb07770eac0cef1697ac35ea384091
avanteijlingen <57176642+avanteijlingen@users.noreply.github.com>
2024-05-08 16:08:22 +0100

    Update Dipole.py

--- Dipole/Dipole.py
+++ Dipole/Dipole.py
@@ -10,15 +10,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 H2O = molecule("H2O")
+H2O.positions += np.random.random((3,3))
+#H2O.positions += 10
+
+COG = H2O.positions.mean(axis=0)
 
 xaxis = 1
 yaxis = 2
 
+
 plt.scatter(H2O.positions[:,xaxis], H2O.positions[:,yaxis])
 print(H2O.get_chemical_symbols())
-charges = np.array([-1, 0.5, 0.5])
+charges = np.array([[-1, 0.5, 0.5]])
 
-dipole = charges*H2O.positions
+dipole = charges.T * H2O.positions
 print(dipole)
 
 dipole = dipole.sum(axis=0)
@@ -27,9 +32,11 @@ print(dipole)
 # Find the direction to draw the arrow
 
 if np.sum(np.abs(H2O.positions[0] - dipole)) > np.sum(np.abs(H2O.positions[0])):
-    plt.arrow(dipole[xaxis], dipole[yaxis], -dipole[xaxis], -dipole[yaxis], width= 0.02, label="dipole")
+    plt.arrow(COG[xaxis], COG[yaxis], -dipole[xaxis], -dipole[yaxis], width= 0.02, label="dipole")
+    print("a")
 else:
-    plt.arrow(0, 0, dipole[xaxis], dipole[yaxis], width= 0.02, label="dipole")
+    plt.arrow(COG[xaxis], COG[yaxis], dipole[xaxis], dipole[yaxis], width= 0.02, label="dipole")
+    print("b")
 
 plt.title("The convention in chemistry is that the arrow representing the dipole moment goes from positive to negative.")
 
